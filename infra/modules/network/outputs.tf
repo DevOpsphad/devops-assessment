551d7a23e9c67c9caba9143ef5f834fd2497b794
DevOpsphad <mohiniphad78@gmail.com>
2026-07-05 23:32:04 +0530

    addedterraform network module

---
+++ infra/modules/network/outputs.tf
@@ -0,0 +1,20 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "Public Subnet IDs"
+  value = [
+    aws_subnet.public_subnet_1.id,
+    aws_subnet.public_subnet_2.id
+  ]
+}
+
+output "private_subnet_ids" {
+  description = "Private Subnet IDs"
+  value = [
+    aws_subnet.private_subnet_1.id,
+    aws_subnet.private_subnet_2.id
+  ]
+}
