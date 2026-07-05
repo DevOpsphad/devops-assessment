919c69a7bd9d759008af6a74020800412cec6079
DevOpsphad <mohiniphad78@gmail.com>
2026-07-06 00:08:24 +0530

    Completed network module

---
+++ infra/envs/dev/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+module "network" {
+  source = "../../modules/network"
+
+  vpc_cidr              = var.vpc_cidr
+  public_subnet_1_cidr  = var.public_subnet_1_cidr
+  public_subnet_2_cidr  = var.public_subnet_2_cidr
+  private_subnet_1_cidr = var.private_subnet_1_cidr
+  private_subnet_2_cidr = var.private_subnet_2_cidr
+
+  availability_zone_1 = var.availability_zone_1
+  availability_zone_2 = var.availability_zone_2
+}
